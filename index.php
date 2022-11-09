8b903510b3675f6d929c1219ea80aeeffc0d25e0
Mamokgolwane Moloi <108424497+mamsieCodess@users.noreply.github.com>
2022-11-09 22:40:50 +0200

    Update index
    Decided to use the index file to only include the different pages of the app

--- index.php
+++ index.php
@@ -1,81 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@900&display=swap" rel="stylesheet">
-    <style>
-        body{
-            font-family: 'Lato', sans-serif;
-        }
-        #signUpForm {
-            background-color: white;
-            background-image: url('./includes/images/background1.jpg');
-            opacity: 0.9;
-            background-size: cover;
-            text-align: center;
-            width: 400px;
-            height: 500px;
-            margin: 0 auto;
-        }
-        .logo{
-            color:white;
-        }
-
-        input {
-            width: 50%;
-            height: 25px;
-            padding: 5px;
-            border-radius: 5px;
-            border: none;
-            margin-bottom: 25px;
-            opacity: 0.9;
-        }
-        #signUpBtn{
-            background-color: rgb(0,0,250);
-            color:white;
-            font-weight: 600;
-        }
-        #signUpBtn:hover{
-        background-color: rgb(0,0,180);
-       
-      }
-
-        
-    </style>
-</head>
-
-<body>
-    <?php if(!$_POST){ ?> 
-
-    <form id="signUpForm" action="" method="post">
-        <h5 class="logo">escape</h5>
-        <h2>Lets book your next vacation</h2>
-        <input type="email" name="username" placeholder="Username" required>
-
-        <input type="password" name="password" placeholder="Password" required>
-
-        <input type="submit" id="signUpBtn" name="signUpBtn" value="Sign Up">
-    </form>
+    <title>Document</title>
     
-    <?php }
-
+</head>
+<body>
+    <!--switching between the login page and the homepage-->
+    <?Php
+    if(!$_POST){
+        include "./template/login.php";
+    }
     else{
-
-         require_once __DIR__ . "/template/homePage.php";
+        include "./template/home.php";
     }
     ?>
-
-
-
-
-</body>
-
-</html>
+   
 </body>
 </html>
